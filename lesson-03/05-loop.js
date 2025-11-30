ef75d26baad20f33c8427a47df4e2c2e26d3d3bb
Nhu <aquarius3294@gmail.com>
2025-11-30 18:35:37 +0700

    last commit

--- lesson-03/05-loop.js
+++ lesson-03/05-loop.js
@@ -1,43 +1,44 @@
 // 1.
-// let sum = 0
-// for (let i = 1; i<=100; i++){
-//     sum = sum + i
+let sum = 0
+for (let i = 1; i<=100; i++){
+    sum = sum + i
 
-// }
-// console.log(sum);
+}
+console.log(sum);
 
 
 // 2.
-// let arr = [] 
-// for (let i = 2; i <= 9; i++ )
-// for (let j = 1; j <= 10; j++ )
-//     arr.push(i*j)
-// console.log("bảng cửu chương:", arr);
+let arr = [] 
+for (let i = 2; i <= 9; i++ )
+for (let j = 1; j <= 10; j++ )
+    arr.push(i*j)
+console.log("bảng cửu chương:", arr);
 
 
 // 3.
-// let arr = []
-// for (let i = 1; i <= 99; i += 2) {
-//     arr.push(i)
-// }
-// console.log(arr);
+let arr = []
+for (let i = 1; i <= 99; i += 2) {
+    arr.push(i)
+}
+console.log(arr);
 
 // 4. 
-// const email = []
-// for (let i = 1; i <=10; i++){
-//     email.push(`user${i}@example.com`)
-// };
-// console.log(email);
+const email = []
+for (let i = 1; i <=10; i++){
+    email.push(`user${i}@example.com`)
+};
+console.log(email);
+
 
 5.
 let sum = 0
 let revenue = []
 const detail = [
 { month: 1, total: 100},
-{ month: 2, total: 120},
+{ month: 2, total: 150},
 { month: 3, total: 140},
 { month: 4, total: 160},
-{ month: 5, total: 180},
+{ month: 5, total: 150},
 { month: 6, total: 100},
 { month: 7, total: 100}
 ];
@@ -47,3 +48,4 @@ sum = sum + detail[i].total}
 revenue.push(sum);
 console.log("tong doanh thu:", revenue);
 
+ 
--- lesson-03/06-function.js
+++ lesson-03/06-function.js
@@ -17,4 +17,30 @@ function findMin(a, b, c) {
 };
 
 findMin(9, 6);
-findMin(1, 2)
+findMin(1, 2);
+
+
+3.
+const result = [];
+let students = [
+    { Name: "Như", Score: 8 },
+    { Name: "Cơ", Score: 9 },
+    { Name: "Hoàng", Score: 6.75 },
+    { Name: "Châu", Score: 7.5 }
+];
+function getTopStudents(students, threshold) {
+    for (let s = 0; s < students.length; s++) {
+        if (students[s].Score > 7.25)
+            result.push(students[s].Name)
+    }
+};
+getTopStudents(students, 7.75);
+console.log(result);
+
+4.
+function calculateInterest (principal,rate, years){
+let total = principal + principal*rate*years/100;
+return total
+};
+total = calculateInterest(100000000,6.5,8);
+console.log(total);
